275cc0248a213d94ded17d2e41f222d6a3c22756
Supreet Aradhya <fcb4days@gmail.com>
2025-11-09 04:56:28 -0600

    Minor changes

--- Backend/main.py
+++ Backend/main.py
@@ -52,6 +52,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", 80)),
+        port=int(os.getenv("PORT", 8000)),
         reload=False
     )
